175ee5b51e90050b23543eeb62961492dc851f5c
AshGrund <ggrund@outlook.com>
2026-06-29 13:51:04 +0200

    Set up variable saving
    set up a way to save variables that can be changed to impact the current from the past.

--- sim-game-jam-2026---chain-reaction/Scripts/Scene Manager.gd
+++ sim-game-jam-2026---chain-reaction/Scripts/Scene Manager.gd
@@ -1,27 +1,40 @@
 extends Node
 
-var dir := DirAccess.open("res://Scenes")
+var sceneDir := DirAccess.open("res://Scenes")
+var saveDir := DirAccess.open("res://Saves")
 var scenes:Dictionary
 var currentScene:Node
+var variables := ConfigFile.new()
 
 
 # loads all the scenes in advance so that they can be instantiated later on
 func _ready():
 	# get all the files in the scene folder
-	if dir == null: printerr("Could not open folder"); return
-	dir.list_dir_begin()
+	if sceneDir == null: printerr("Could not open the Scenes folder"); return
+	sceneDir.list_dir_begin()
 	
 	# load all the files and add them to the scenes array for later use
-	for file: String in dir.get_files():
-		var scene := load(dir.get_current_dir() + "/" + file)
-		print(file + "test")
+	for file: String in sceneDir.get_files():
+		var scene := load(sceneDir.get_current_dir() + "/" + file)
 		scenes[file.replace(".tscn", "")] = scene
 	
-	# load the starting scene (change number as needed)
+	# try to load the scene save file
+	if saveDir == null: printerr("Could not open the Saves folder"); return
+	saveDir.list_dir_begin()
+	var err = variables.load(saveDir.get_current_dir() + "/" + "Scene Changes.cfg")
+	
+	# there is no save file yet, make one
+	if err != OK:
+		variables.save(saveDir.get_current_dir() + "/" + "Scene Changes.cfg")
+	
+	# instantiate the starting scene (change name as needed)
 	SwitchScene("Test 1")
 
-# deleting the currently shown scene and instantiating the next one
+# save variables, delete the currently shown scene and instantiate the next one
 func SwitchScene(scene):
+	# save the variables
+	variables.save(saveDir.get_current_dir() + "/" + "Scene Changes.cfg")
+	
 	# delete current scene
 	if(currentScene):
 		currentScene.queue_free()
@@ -29,3 +42,12 @@ func SwitchScene(scene):
 	# initialize new scene and make them a child of this object
 	currentScene = scenes[scene].instantiate()
 	self.add_child(currentScene)
+
+func GetVariable(scene, object):
+	if !variables.get_value(scene, object):
+		variables.set_value(scene, object, 0)
+	
+	return variables.get_value(scene, object)
+
+func ChangeVariable(scene, object, value):
+	variables.set_value(scene, object, value)
