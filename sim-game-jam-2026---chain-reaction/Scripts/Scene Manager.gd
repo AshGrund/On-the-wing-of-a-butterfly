cfdf599c66c2acd33dab8d9946310c72b6af79b3
AshGrund <ggrund@outlook.com>
2026-06-30 14:24:04 +0200

    Loading and saving generalisation
    Loading Scenes is now more generalized.
    
    This way I can have scenes in scenes (Like for the Main Menu + Settings and a Zoom in on objects)

--- sim-game-jam-2026---chain-reaction/Scripts/Scene Manager.gd
+++ sim-game-jam-2026---chain-reaction/Scripts/Scene Manager.gd
@@ -1,6 +1,10 @@
 extends Node
 
-var sceneDir := DirAccess.open("res://Scenes")
+@export var sceneDirName: String
+@export var saveName: String
+@export var loadFirstName: String
+
+var sceneDir
 var saveDir := DirAccess.open("res://Saves")
 var scenes:Dictionary
 var currentScene:Node
@@ -9,6 +13,7 @@ var variables := ConfigFile.new()
 
 # loads all the scenes in advance so that they can be instantiated later on
 func _ready():
+	sceneDir = DirAccess.open(sceneDirName)
 	# get all the files in the scene folder
 	if sceneDir == null: printerr("Could not open the Scenes folder"); return
 	sceneDir.list_dir_begin()
@@ -19,35 +24,39 @@ func _ready():
 		scenes[file.replace(".tscn", "")] = scene
 	
 	# try to load the scene save file
-	if saveDir == null: printerr("Could not open the Saves folder"); return
-	saveDir.list_dir_begin()
-	var err = variables.load(saveDir.get_current_dir() + "/" + "Scene Changes.cfg")
+	if saveName:
+		if saveDir == null: printerr("Could not open the Saves folder"); return
+		saveDir.list_dir_begin()
+		var err = variables.load(saveDir.get_current_dir() + "/" + saveName + ".cfg")
 	
-	# there is no save file yet, make one
-	if err != OK:
-		variables.save(saveDir.get_current_dir() + "/" + "Scene Changes.cfg")
+		# there is no save file yet, make one
+		if err != OK:
+			variables.save(saveDir.get_current_dir() + "/" + saveName + ".cfg")
 	
 	# instantiate the starting scene (change name as needed)
-	SwitchScene("Test 1")
+	SwitchScene(loadFirstName)
 
 # save variables, delete the currently shown scene and instantiate the next one
 func SwitchScene(scene):
 	# save the variables
-	variables.save(saveDir.get_current_dir() + "/" + "Scene Changes.cfg")
+	if saveName:
+		variables.save(saveDir.get_current_dir() + "/" + saveName + ".cfg")
 	
-	# delete current scene
-	if(currentScene):
-		currentScene.queue_free()
+		# delete current scene
+		if(currentScene):
+			currentScene.queue_free()
 	
-	# initialize new scene and make them a child of this object
-	currentScene = scenes[scene].instantiate()
-	self.add_child(currentScene)
+		# initialize new scene and make them a child of this object
+		currentScene = scenes[scene].instantiate()
+		self.add_child(currentScene)
 
 func GetVariable(scene, object):
-	if !variables.get_value(scene, object):
-		variables.set_value(scene, object, 0)
+	if saveName:
+		if !variables.get_value(scene, object):
+			variables.set_value(scene, object, "base")
 	
-	return variables.get_value(scene, object)
+		return variables.get_value(scene, object)
 
 func ChangeVariable(scene, object, value):
-	variables.set_value(scene, object, value)
+	if saveName:
+		variables.set_value(scene, object, value)
